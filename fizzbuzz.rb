32a53901c9caba13dc25910bc5dd1d78e44c5bcd
Molly Seither <molly.seither@centre.edu>
2018-07-11 20:14:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    return "FizzBuzz"
+    "FizzBuzz"
   elsif int % 5 == 0
-    return "Buzz"
+    "Buzz"
   elsif int % 3 == 0
-    return "Fizz"
+    "Fizz"
   else
     return nil
   end
