5c2c0cb9cb73328f2bc5d88dd62b292a7b74e0a3
Molly Seither <molly.seither@centre.edu>
2018-07-11 19:59:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,11 @@
-require 'rspec'
-
-def fizz_buzz(int)
-  if (int%3==0 && int%5==0)
-    "FizzBuzz"
-  elsif (int%3==0)
-    return "Fizz"
-  elsif (int%5==0)
+def fizzbuzz(int)
+  if (int % 3 == 0 && int % 5 == 0)
+    return "FizzBuzz"
+  elsif (int % 5  == 0)
     return "Buzz"
+  elsif (int % 3 == 0)
+    return "Fizz"
   else
-    return int
- end
+    return nil
+  end
+end
